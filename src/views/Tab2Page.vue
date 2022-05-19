db2874ec5108b90213feb45cd61bb8fb0e49702e
Luis Ruiz <0224451@up.edu.mx>
2022-05-19 07:52:12 -0500

    Deploy User and Generate Codes

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,78 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Monitoreo de Usuarios</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-grid>
+          <ion-row>
+            <ion-col>
+              <div>Clave</div>
+            </ion-col>
+            <ion-col>
+              <div>Nombre</div>
+            </ion-col>
+            <ion-col>
+              <div>Apellido</div>
+            </ion-col>
+            <ion-col>
+              <div>Carrera</div>
+            </ion-col>
+            <ion-col>
+              <div>Semestre</div>
+            </ion-col>
+          </ion-row>
+          <ion-row v-for="(item, index) in listaUsuarios" :key="index">
+            <ion-col>
+              <div>{{listaKeys[index]}}</div>
+            </ion-col>
+            <ion-col>
+              <div>{{item.nombre}}</div>
+            </ion-col>
+            <ion-col>
+              <div>{{item.apellido}}</div>
+            </ion-col>
+            <ion-col>
+              <div>{{item.carrera}}</div>
+            </ion-col>
+            <ion-col>
+              <div>{{item.semestre}}</div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol } from '@ionic/vue';
+import { getDatabase, ref, onValue } from "firebase/database"
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonGrid, IonRow, IonCol },
+  mounted() {
+    const db = getDatabase();
+    const starCountRef = ref(db, "usuario/");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    var cont = 0
+      snapshot.forEach(element => {
+        this.listaKeys[cont]=element.key
+        this.listaUsuarios[cont] = element.toJSON()
+        cont++
+      });
+    });
+    console.log("lista de usuarios", this.listaUsuarios)
+  },
+
+  data(){ return{
+  listaUsuarios: [{status:"", usuario:""}],
+  listaKeys:[]
+  }}
+
 });
 </script>
